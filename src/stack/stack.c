fdece18292352b07c97a6e035468b2bd4d244b5f
PavelLukl <pavellukl@ids.company>
2023-10-06 19:06:54 +0200

    feat: reusable stack /wo tests

---
+++ src/stack/stack.c
@@ -0,0 +1,76 @@
+#include <stack.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+Stack* stack_init() {
+    Stack* stack = malloc(sizeof(stack));
+    if (stack == NULL) return NULL;
+
+    stack->size = INITIAL_N_ITEMS;
+    stack->items = malloc(stack->size * sizeof(void*));
+    stack->length = 0;
+    stack->topIndex = -1;
+
+    return stack;
+}
+
+void* stack_pop(Stack* stack) {
+    if (stack->length == 0) return NULL;
+
+    stack->topIndex--;
+    stack->length--;
+
+    return stack->items[stack->topIndex + 1];
+}
+
+bool stack_push(Stack* stack, void* data) {
+    if (stack->size == stack->length) {
+        void** buffer =
+            realloc(stack->items, (stack->size * 2) * sizeof(void*));
+        if (buffer == NULL) {
+            free(stack->items);
+            return false;
+        }
+
+        stack->size *= 2;
+        stack->items = buffer;
+    }
+
+    stack->topIndex++;
+    stack->items[stack->topIndex] = data;
+
+    stack->length++;
+
+    return true;
+}
+
+void* stack_top(Stack* stack) {
+    if (stack->length == 0) return NULL;
+    return stack->items[stack->topIndex];
+}
+
+bool stack_remove_elements(Stack* stack) {
+    while (stack_pop(stack))
+        ;
+
+    stack->topIndex = -1;
+    stack->length = 0;
+
+    void** buffer = realloc(stack->items, INITIAL_N_ITEMS * sizeof(void*));
+    if (buffer == NULL) {
+        free(stack->items);
+        return false;
+    }
+
+    stack->size = INITIAL_N_ITEMS;
+    stack->items = buffer;
+
+    return true;
+}
+
+void stack_free(Stack* stack) {
+    free(stack->items);
+    free(stack);
+}
+
+bool is_empty(Stack* stack) { return stack->length == 0; }
---
+++ src/stack/stack.h
@@ -0,0 +1,78 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+/**
+ * @brief Stack structure type
+ *
+ */
+typedef struct {
+    /// @brief index of the top stack item in items array
+    int topIndex;
+
+    /// @brief current length of the items array
+    int length;
+
+    /// @brief current allocated size of the items array
+    int size;
+
+    /// @brief array of stack items which contain saved pointers
+    void** items;
+} Stack;
+
+/// @brief initial allocated size of the items array
+const int INITIAL_N_ITEMS = 8;
+
+/**
+ * @brief procedure to initialize a new stack
+ *
+ * @return Stack* returns a pointer to the created Stack struct
+ */
+Stack* stack_init();
+
+/**
+ * @brief stack pop function, removes the top item of the stack, doesn't do
+ * anything if stack is empty
+ *
+ * @param stack pointer to the stack which should be popped
+ * @return void* returns pointer to the data of the popped element or NULL if
+ * stack is empty
+ */
+void* stack_pop(Stack* stack);
+
+/**
+ * @brief stack push function, adds an item to the top of the stack, if alloced
+ * size for the array is full reallocates memory
+ *
+ * @param stack pointer to the stack to operate upon
+ * @param data  pointer to the data which should be added onto the stack
+ * @return true if push was successful
+ * @return false if push was unsuccessful (fails due to no memory while
+ * allocating)
+ */
+bool stack_push(Stack* stack, void* data);
+
+/**
+ * @brief function which returns the data pointer of the top item
+ *
+ * @param stack pointer to the stack
+ * @return void* NULL if stack is empty, otherwise pointer to the data of the
+ * top item
+ */
+void* stack_top(Stack* stack);
+
+/**
+ * @brief Removes all elements of the stack and re-initializes the stack
+ *
+ * @param stack pointer to the stack
+ * @return true if re-initialization was successful
+ * @return false if re-initialization was not successful
+ */
+bool stack_remove_elements(Stack* stack);
+
+/**
+ * @brief Removes all elements and frees the stack completely
+ * ONLY use if you wish to completely dispose of the stack
+ *
+ * @param stack pointer to the stack
+ */
+void stack_free(Stack* stack);
