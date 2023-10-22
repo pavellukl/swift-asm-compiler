4d9f2a5d664e19ef96a28e15c6c43b8bdbe23b74
denyzzko <126917003+denyzzko@users.noreply.github.com>
2023-10-22 20:12:13 +0200

    Version 0.5

--- src/lexical_analysis/lexical_analysis.h
+++ src/lexical_analysis/lexical_analysis.h
@@ -1,8 +1,127 @@
 #ifndef LEXICAL_ANALYSIS_H
 #define LEXICAL_ANALYSIS_H
 
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-// Write your definitions here
+typedef enum {
+    START,
+    DIV,
+    LINE_COMMENT,
+    BLOCK_COMMENT,
+    BLOCK_COMMENT_END,
+    ADD,
+    SUB,
+    ARROW,
+    NEW_LINE,
+    END_OF_FILE,
+    MUL,
+    EXCL_MARK,
+    NOT_EQUAL,
+    ASSIGN,
+    EQUAL,
+    GREATER,
+    GREATER_EQUAL,
+    LESSER,
+    LESSER_EQUAL,
+    L_BRACKET,
+    R_BRACKET,
+    L_CRLY_BRACKET,
+    R_CRLY_BRACKET,
+    COLON,
+    COMA,
+    IDENTIF,
+    NUM,
+    DECIMAL_POINT,
+    DECIMAL_VALUE,
+    EXP_MARK,
+    EXP_SIGN,
+    EXPONENT,
+    STRING,
+    STRING_END,
+    ESCAPE_SEQUENCE,
+    UNICODE_SEQUENCE,
+    UNICODE_VALUE,
+    QUESTION_MARK,
+    NIL_COALESCING
+} AutomatState;
+
+typedef enum {
+    TOKEN_LINE_COMMENT,
+    TOKEN_BLOCK_COMMENT,      // to do nested comments
+    TOKEN_IDENTIF,
+    TOKEN_KEYWORD_DOUBLE,
+    TOKEN_KEYWORD_DOUBLE_NIL,
+    TOKEN_KEYWORD_ELSE,
+    TOKEN_KEYWORD_FUNC,
+    TOKEN_KEYWORD_IF,
+    TOKEN_KEYWORD_INT,
+    TOKEN_KEYWORD_INT_NIL,
+    TOKEN_KEYWORD_LET,
+    TOKEN_KEYWORD_NIL,
+    TOKEN_KEYWORD_RETURN,
+    TOKEN_KEYWORD_STRING,
+    TOKEN_KEYWORD_STRING_NIL,
+    TOKEN_KEYWORD_VAR,
+    TOKEN_KEYWORD_WHILE,
+    TOKEN_FUNC_READ_STRING,
+    TOKEN_FUNC_READ_INT,
+    TOKEN_FUNC_READ_DOUBLE,
+    TOKEN_FUNC_WRITE,
+    TOKEN_FUNC_INT_2_DOUBLE,
+    TOKEN_FUNC_DOUBLE_2_INT,
+    TOKEN_FUNC_LENGTH,
+    TOKEN_FUNC_SUBSTRING,
+    TOKEN_FUNC_ORD,
+    TOKEN_FUNC_CHR,
+    TOKEN_NUM_W_EXP,
+    TOKEN_INT_NUM,
+    TOKEN_FLOAT_NUM,         //to do nested strings
+    TOKEN_STRING,            //to do unicode (and escape) value evaluation
+    TOKEN_ADD,
+    TOKEN_SUB,
+    TOKEN_MUL,
+    TOKEN_DIV,
+    TOKEN_NIL_COALESCING,
+    TOKEN_EXCL_MARK,
+    TOKEN_NOT_EQUAL,
+    TOKEN_EQUAL,
+    TOKEN_ASSIGN,
+    TOKEN_GREATER,
+    TOKEN_GREATER_EQUAL,
+    TOKEN_LESSER,
+    TOKEN_LESSER_EQUAL,
+    TOKEN_L_BRACKET,
+    TOKEN_R_BRACKET,
+    TOKEN_L_CRLY_BRACKET,
+    TOKEN_R_CRLY_BRACKET,
+    TOKEN_COLON,
+    TOKEN_COMA,
+    TOKEN_ARROW,
+    TOKEN_NEW_LINE,
+    TOKEN_END_OF_FILE
+} TokenType;
+
+typedef struct {
+    TokenType type;
+    int line_index;
+
+    union {                    //to do malloc error check
+        char* string;          //to do lex errors
+        int int_value;         //to do free(token)
+        double float_value;
+    } value;
+} TokenData;
+
+
+TokenData is_keyword(char* keyword);
+
+TokenData is_built_in_function(char* keyword);
+
+void _skip_whitespaces(FILE* input);
+
+TokenData get_next_token(FILE *input, int line_counter);
 
 #endif /* LEXICAL_ANALYSIS_H */
