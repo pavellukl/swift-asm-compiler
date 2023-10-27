a45297f35a3ade16cbbc1a348a55d3baebd32cae
jklanica <jklanica@email.cz>
2023-10-27 18:13:07 +0200

    feat: token examples

--- src/lexical_analysis/lexical_analysis.h
+++ src/lexical_analysis/lexical_analysis.h
@@ -49,57 +49,57 @@ typedef enum {
 } AutomatState;
 
 typedef enum {
-    TOKEN_LINE_COMMENT,
-    TOKEN_BLOCK_COMMENT,      // to do nested comments
+    TOKEN_LINE_COMMENT,         
+    TOKEN_BLOCK_COMMENT,                                   // TODO: nested comments
     TOKEN_IDENTIF,
-    TOKEN_KEYWORD_DOUBLE,
-    TOKEN_KEYWORD_DOUBLE_NIL,
-    TOKEN_KEYWORD_ELSE,
-    TOKEN_KEYWORD_FUNC,
-    TOKEN_KEYWORD_IF,
-    TOKEN_KEYWORD_INT,
-    TOKEN_KEYWORD_INT_NIL,
-    TOKEN_KEYWORD_LET,
-    TOKEN_KEYWORD_NIL,
-    TOKEN_KEYWORD_RETURN,
-    TOKEN_KEYWORD_STRING,
-    TOKEN_KEYWORD_STRING_NIL,
-    TOKEN_KEYWORD_VAR,
-    TOKEN_KEYWORD_WHILE,
-    TOKEN_FUNC_READ_STRING,
-    TOKEN_FUNC_READ_INT,
-    TOKEN_FUNC_READ_DOUBLE,
-    TOKEN_FUNC_WRITE,
-    TOKEN_FUNC_INT_2_DOUBLE,
-    TOKEN_FUNC_DOUBLE_2_INT,
-    TOKEN_FUNC_LENGTH,
-    TOKEN_FUNC_SUBSTRING,
-    TOKEN_FUNC_ORD,
-    TOKEN_FUNC_CHR,
-    TOKEN_NUM_W_EXP,
-    TOKEN_INT_NUM,
-    TOKEN_FLOAT_NUM,         //to do nested strings
-    TOKEN_STRING,            //to do unicode (and escape) value evaluation
-    TOKEN_ADD,
-    TOKEN_SUB,
-    TOKEN_MUL,
-    TOKEN_DIV,
-    TOKEN_NIL_COALESCING,
-    TOKEN_EXCL_MARK,
-    TOKEN_NOT_EQUAL,
-    TOKEN_EQUAL,
-    TOKEN_ASSIGN,
-    TOKEN_GREATER,
-    TOKEN_GREATER_EQUAL,
-    TOKEN_LESSER,
-    TOKEN_LESSER_EQUAL,
-    TOKEN_L_BRACKET,
-    TOKEN_R_BRACKET,
-    TOKEN_L_CRLY_BRACKET,
-    TOKEN_R_CRLY_BRACKET,
-    TOKEN_COLON,
-    TOKEN_COMA,
-    TOKEN_ARROW,
+    TOKEN_KEYWORD_INT,          // Int
+    TOKEN_KEYWORD_INT_NIL,      // Int?
+    TOKEN_KEYWORD_DOUBLE,       // Double
+    TOKEN_KEYWORD_DOUBLE_NIL,   // Double?
+    TOKEN_KEYWORD_STRING,       // String
+    TOKEN_KEYWORD_STRING_NIL,   // String?
+    TOKEN_KEYWORD_NIL,          // Nil
+    TOKEN_KEYWORD_LET,          // let
+    TOKEN_KEYWORD_VAR,          // var
+    TOKEN_KEYWORD_FUNC,         // func
+    TOKEN_KEYWORD_IF,           // if
+    TOKEN_KEYWORD_ELSE,         // else
+    TOKEN_KEYWORD_WHILE,        // while
+    TOKEN_KEYWORD_RETURN,       // return
+    TOKEN_FUNC_READ_INT,        // readInt
+    TOKEN_FUNC_READ_DOUBLE,     // readDouble
+    TOKEN_FUNC_READ_STRING,     // readString
+    TOKEN_FUNC_INT_2_DOUBLE,    // Int2Double
+    TOKEN_FUNC_DOUBLE_2_INT,    // Double2Int
+    TOKEN_FUNC_WRITE,           // write
+    TOKEN_FUNC_LENGTH,          // length
+    TOKEN_FUNC_SUBSTRING,       // substring
+    TOKEN_FUNC_ORD,             // ord
+    TOKEN_FUNC_CHR,             // chr
+    TOKEN_NUM_W_EXP,            // e.g. 1.23E+10
+    TOKEN_INT_NUM,              // e.g. 6
+    TOKEN_FLOAT_NUM,            // e.g. 1.2                // TODO: nested strings
+    TOKEN_STRING,               // e.g. "any string"       // TODO: unicode (and escape) value evaluation
+    TOKEN_ADD,                  // +
+    TOKEN_SUB,                  // -
+    TOKEN_MUL,                  // *
+    TOKEN_DIV,                  // /
+    TOKEN_NIL_COALESCING,       // ??
+    TOKEN_EXCL_MARK,            // !
+    TOKEN_NOT_EQUAL,            // !=
+    TOKEN_EQUAL,                // ==
+    TOKEN_ASSIGN,               // =
+    TOKEN_GREATER,              // >
+    TOKEN_GREATER_EQUAL,        // >=
+    TOKEN_LESSER,               // <
+    TOKEN_LESSER_EQUAL,         // <=
+    TOKEN_L_BRACKET,            // (
+    TOKEN_R_BRACKET,            // )
+    TOKEN_L_CRLY_BRACKET,       // {
+    TOKEN_R_CRLY_BRACKET,       // }
+    TOKEN_COLON,                // |
+    TOKEN_COMA,                 // ,
+    TOKEN_ARROW,                // ->
     TOKEN_NEW_LINE,
     TOKEN_END_OF_FILE
 } TokenType;
@@ -108,9 +108,9 @@ typedef struct {
     TokenType type;
     int line_index;
 
-    union {                    //to do malloc error check
-        char* string;          //to do lex errors
-        int int_value;         //to do free(token)
+    union {                    // TODO: malloc error check
+        char* string;          // TODO: lex errors
+        int int_value;         // TODO: free(token)
         double float_value;
     } value;
 } TokenData;
