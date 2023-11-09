53fe6b75ba17c2aa2d75201a0fe2a88d835238c0
denyzzko <126917003+denyzzko@users.noreply.github.com>
2023-11-10 00:53:11 +0100

    Update token.h

--- src/scanner/token.h
+++ src/scanner/token.h
@@ -2,73 +2,72 @@
 #define TOKEN_H
 
 typedef enum {
-    TOKEN_LINE_COMMENT,         
-    TOKEN_BLOCK_COMMENT,                                   // TODO: nested comments
+    TOKEN_LINE_COMMENT,
+    TOKEN_BLOCK_COMMENT,
     TOKEN_IDENTIF,
-    TOKEN_KEYWORD_INT,          // Int
-    TOKEN_KEYWORD_INT_NIL,      // Int?
-    TOKEN_KEYWORD_DOUBLE,       // Double
-    TOKEN_KEYWORD_DOUBLE_NIL,   // Double?
-    TOKEN_KEYWORD_STRING,       // String
-    TOKEN_KEYWORD_STRING_NIL,   // String?
-    TOKEN_KEYWORD_BOOL,         // Bool
-    TOKEN_KEYWORD_BOOL_NIL,     // Bool?
-    TOKEN_KEYWORD_NIL,          // nil
-    TOKEN_KEYWORD_LET,          // let
-    TOKEN_KEYWORD_VAR,          // var
-    TOKEN_KEYWORD_FUNC,         // func
-    TOKEN_KEYWORD_IF,           // if
-    TOKEN_KEYWORD_ELSE,         // else
-    TOKEN_KEYWORD_WHILE,        // while
-    TOKEN_KEYWORD_RETURN,       // return
-    TOKEN_FUNC_READ_INT,        // readInt
-    TOKEN_FUNC_READ_DOUBLE,     // readDouble
-    TOKEN_FUNC_READ_STRING,     // readString
-    TOKEN_FUNC_INT_2_DOUBLE,    // Int2Double
-    TOKEN_FUNC_DOUBLE_2_INT,    // Double2Int
-    TOKEN_FUNC_WRITE,           // write
-    TOKEN_FUNC_LENGTH,          // length
-    TOKEN_FUNC_SUBSTRING,       // substring
-    TOKEN_FUNC_ORD,             // ord
-    TOKEN_FUNC_CHR,             // chr
-    TOKEN_NUM_W_EXP,            // e.g. 1.23E+10
-    TOKEN_INT_NUM,              // e.g. 6
-    TOKEN_FLOAT_NUM,            // e.g. 1.2                // TODO: nested strings
-    TOKEN_NUMBER,
-    TOKEN_STRING,               // e.g. "any string"       // TODO: unicode (and escape) value evaluation
-    TOKEN_ADD,                  // +
-    TOKEN_SUB,                  // -
-    TOKEN_MUL,                  // *
-    TOKEN_DIV,                  // /
-    TOKEN_NIL_COALESCING,       // ??
-    TOKEN_EXCL_MARK,            // !
-    TOKEN_NOT_EQUAL,            // !=
-    TOKEN_EQUAL,                // ==
-    TOKEN_ASSIGN,               // =
-    TOKEN_GREATER,              // >
-    TOKEN_GREATER_EQUAL,        // >=
-    TOKEN_LESSER,               // <
-    TOKEN_LESSER_EQUAL,         // <=
-    TOKEN_L_BRACKET,            // (
-    TOKEN_R_BRACKET,            // )
-    TOKEN_L_CRLY_BRACKET,       // {
-    TOKEN_R_CRLY_BRACKET,       // }
-    TOKEN_COLON,                // :
-    TOKEN_COMA,                 // ,
-    TOKEN_ARROW,                // ->
-    TOKEN_UNDERSCORE,           // _
-    TOKEN_NEW_LINE,
+    TOKEN_KEYWORD_DOUBLE,        // Double
+    TOKEN_KEYWORD_DOUBLE_NIL,    // Double?
+    TOKEN_KEYWORD_ELSE,          // else
+    TOKEN_KEYWORD_FUNC,          // func
+    TOKEN_KEYWORD_IF,            // if
+    TOKEN_KEYWORD_INT,           // Int
+    TOKEN_KEYWORD_INT_NIL,       // Int?
+    TOKEN_KEYWORD_LET,           // let
+    TOKEN_KEYWORD_NIL,           // nil
+    TOKEN_KEYWORD_RETURN,        // return
+    TOKEN_KEYWORD_STRING,        // String
+    TOKEN_KEYWORD_STRING_NIL,    // String?
+    TOKEN_KEYWORD_VAR,           // var
+    TOKEN_KEYWORD_WHILE,         // while
+    TOKEN_KEYWORD_TRUE,          // true
+    TOKEN_KEYWORD_FALSE,         // false
+    TOKEN_KEYWORD_BOOL,          // Bool
+    TOKEN_KEYWORD_BOOL_NIL,      // Bool?
+    TOKEN_NUMBER,                // number                     //TODO: nested strings
+    TOKEN_STRING,                // e.g. "any string"          //TODO: unicode (and escape) value evaluation
+    TOKEN_ADD,                   // +
+    TOKEN_SUB,                   // -
+    TOKEN_MUL,                   // *                          //TODO: lookahead
+    TOKEN_DIV,                   // /
+    TOKEN_NIL_COALESCING,        // ??
+    TOKEN_EXCL_MARK,             // !
+    TOKEN_NOT,                   // !xxx
+    TOKEN_NOT_EQUAL,             // !=
+    TOKEN_EQUAL,                 // ==
+    TOKEN_ASSIGN,                // =
+    TOKEN_GREATER,               // >
+    TOKEN_GREATER_EQUAL,         // >=
+    TOKEN_LESSER,                // <
+    TOKEN_LESSER_EQUAL,          // <=     
+    TOKEN_L_BRACKET,             // (
+    TOKEN_R_BRACKET,             // )
+    TOKEN_L_CRLY_BRACKET,        // {
+    TOKEN_R_CRLY_BRACKET,        // }
+    TOKEN_COLON,                 // :
+    TOKEN_COMA,                  // ,
+    TOKEN_ARROW,                 // ->
+    TOKEN_UNDERSCORE,            // _
+    TOKEN_AND,                   // &&
+    TOKEN_OR,                    // ||
     TOKEN_END_OF_FILE
 } TokenType;
 
+typedef enum {
+    INT,
+    FLOAT
+}TokenNumberType;
+
 typedef struct {
     TokenType type;
+    TokenNumberType num_type;
     int line_index;
-
-    union {                    // TODO: malloc error check
-        char* string;          // TODO: lex errors
-        int int_value;         // TODO: free(token)
+    bool eol_before;
+    
+    union {                    //TODO: malloc error check
+        char* string;          //TODO: lex errors
+        int int_value;         //TODO: free(token)
         double float_value;
+        bool boolean;
     } value;
 } TokenData;
 
