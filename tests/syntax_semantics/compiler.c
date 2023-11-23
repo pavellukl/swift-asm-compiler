e8398929f6e991412df4ace0d70b38ba3bfbb701
jklanica <jklanica@email.cz>
2023-11-23 12:49:01 +0100

    feat: test suites renaiming & colors change

--- tests/syntax_semantics/compiler.c
+++ tests/syntax_semantics/compiler.c
@@ -7,16 +7,16 @@
 #include <time.h>
 #include <stdbool.h>
 
-#define ANSI_COLOR_RED "\x1b[31m"
-#define ANSI_COLOR_GREEN "\x1b[32m"
-#define ANSI_COLOR_BLUE "\x1b[34m"
+#define ANSI_COLOR_RED_ "\x1b[0;31m"
+#define ANSI_COLOR_GREEN "\x1b[0;32m"
+#define ANSI_COLOR_BLUE "\x1b[0;34m"
 #define ANSI_COLOR_RESET "\x1b[0m"
 #define ANSI_CURSOR_BACK_7 "\x1b[7D"
 #define ANSI_CURSOR_UP "\x1b[1A"
 
 #define PASS "[" ANSI_COLOR_GREEN "PASS" ANSI_COLOR_RESET "]"
 #define RUN "[" ANSI_COLOR_BLUE "RUN" ANSI_COLOR_RESET " ]"
-#define ERR "[" ANSI_COLOR_RED "FAIL" ANSI_COLOR_RESET "]"
+#define ERR "[" ANSI_COLOR_RED_ "FAIL" ANSI_COLOR_RESET "]"
 
 Test(syntax_semantics, general) {
     DIR *dir = opendir("./tests/syntax_semantics/test_files");
@@ -46,7 +46,8 @@ Test(syntax_semantics, general) {
         CompilerReturnCode expectedRes;
         fscanf(in, "// %d", (int *)&expectedRes);
 
-        cr_log_info(ANSI_CURSOR_BACK_7 RUN " compile::%s", subdirPtr->d_name);
+        cr_log_info(
+            ANSI_CURSOR_BACK_7 RUN " syntax_semantics::%s", subdirPtr->d_name);
 
         clock_t start = clock();
         CompilerReturnCode res = compile(in, NULL);
@@ -56,12 +57,12 @@ Test(syntax_semantics, general) {
             test_failed = true;
             cr_log_error(
                 ANSI_CURSOR_BACK_7 ANSI_CURSOR_UP ERR
-                " compile::%s. Expected: %d but compiler returned %d",
+                " syntax_semantics::%s. Expected: %d but compiler returned %d",
                 subdirPtr->d_name, expectedRes, res);
         } else {
             float seconds = (float)(end - start) / CLOCKS_PER_SEC;
             cr_log_info(ANSI_CURSOR_UP ANSI_CURSOR_BACK_7 PASS
-                        " compile::%s: (%fs)", subdirPtr->d_name, seconds);
+                    " syntax_semantics::%s: (%fs)", subdirPtr->d_name, seconds);
         }
 
         fclose(in);
