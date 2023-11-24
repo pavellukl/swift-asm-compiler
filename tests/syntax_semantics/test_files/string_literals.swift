2ad4d8e3e19c439013d5898817b0d8dbab21973a
jklanica <jklanica@email.cz>
2023-11-24 17:33:41 +0100

    fix: string literals test

--- tests/syntax_semantics/test_files/string_literals.swift
+++ tests/syntax_semantics/test_files/string_literals.swift
@@ -3,12 +3,10 @@
 let empty = ""
 let deadly = "\" \\n \\r \\t \\t \\r  \" \" \\\\n"
 
-var 0xabc45 = "\u{aFb48c}" 
+var xabc45 = "\u{aFb48c}" 
 let hello = "Ahoj\n\"Sve'te \\\u{22}"
 
-let multiline = """ 
+let multiline = """
 
-        "" Hello  \n""
-  "  \" \n \r \t\t \r \n "
 
 """
