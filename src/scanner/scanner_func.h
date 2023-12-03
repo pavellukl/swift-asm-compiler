e0b5652d68412ae08baf9527ae817f0e1b9aa04a
denyzzko <126917003+denyzzko@users.noreply.github.com>
2023-12-03 23:08:30 +0100

    add: comments

--- src/scanner/scanner_func.h
+++ src/scanner/scanner_func.h
@@ -1,8 +1,8 @@
 /**
  * @file scanner_func.h
- * @brief TODO: file header
+ * @brief header file containing function definitions for scanner
  * 
- * @author your name (you@domain.com)
+ * @author Denis Milistenfer (xmilis00)
  */
 
 #ifndef SCANNER_FUNC_H
@@ -20,111 +20,128 @@
 #include "../helpers/utils.h"
 
 /**
- * @brief TODO:
+ * @brief this functions checks if a word that has been read is a keyword
  * 
- * @param keyword 
- * @return TokenData 
+ * @param keyword pointer to a word that was scanned
+ * @return token for corresponding keyword or TOKEN_IDENTIF 
+ * if passed word does not match with any IFJ23 keywords
  */
 TokenData is_keyword(char *keyword);
 
 /**
- * @brief TODO:
+ * @brief this function skips whitespaces in code until first non-whitespace
+ * character is read and handles '\n'
  * 
- * @param opt 
- * @return true 
- * @return false 
+ * @param opt scanner options struct pointer from which code is being read
+ * @return true if newline was among whitespaces
+ * @return false if newline was not among whitespaces
  */
 bool _skip_whitespaces(ScannerOptions *opt);
 
 /**
- * @brief TODO:
+ * @brief this function removes extra whitespaces in multiline string
+ * according to swift multiline indentation
  * 
- * @param buf 
- * @param count 
+ * @param buf pointer to scanner buffer struct where string is stored
+ * @param count number of whitespaces before closing quotes 
+ * (number of whitespaces that needs to be removed)
  */
 void _remove_extra_whitespaces(ScannerBuffer *buf, int count);
 
 /**
- * @brief TODO:
+ * @brief this function frees allocated memmory and sets 
+ * scanner options structure to it's initialization state
  * 
- * @param opt 
+ * @param opt scanner options struct pointer containing all necessary data
+ * for handling input code IFJ23 
  */
 void scanner_opt_free(ScannerOptions *opt);
 
 /**
- * @brief TODO:
+ * @brief this function initializes all necessary values inside scanner option structure,
+ * allocates memmory for input file and stores it
  * 
- * @param opt 
- * @param file 
- * @return true 
- * @return false 
+ * @param opt scanner options struct pointer containing all necessary data
+ * for handling input code IFJ23 
+ * @param file pointer to input file
+ * @return true if all allocations are successfull
+ * @return false if allocation error occurs
  */
 bool scanner_opt_init(ScannerOptions *opt, FILE *file);
 
 /**
- * @brief TODO:
+ * @brief this function sets scanner option structure to it's initialization state
+ * to allow second run through file
  * 
- * @param opt 
+ * @param opt scanner options struct pointer containing all necessary data
+ * for handling input code IFJ23 
  */
 void scanner_rewind_file(ScannerOptions *opt);
 
 /**
- * @brief TODO:
+ * @brief this function copies value of src token to dest token
  * 
- * @param dest 
- * @param src 
- * @return true 
- * @return false 
+ * @param dest pointer to destination token where value is being copied to
+ * @param src source token which value is being copied from
+ * @return true if everything was successfull
+ * @return false if allocation within clone_string() function was not successfull
  */
 bool clone_token(TokenData *dest, TokenData src);
 
 /**
- * @brief TODO:
+ * @brief this function frees allocated memmory for identifier token value or
+ * token string value 
  * 
- * @param token 
+ * @param token token which memmory needs to be freed
  */
 void free_token(TokenData token);
 
 /**
- * @brief TODO:
+ * @brief this function initializes all necessary values inside scanner buffer struct and
+ * allocates memmory for buffer
  * 
- * @param buf 
- * @return true 
- * @return false 
+ * @param buf pointer to scanner buffer structure
+ * @return true if allocation was successfull
+ * @return false if allocation error occured
  */
 bool scanner_buf_init(ScannerBuffer *buf);
 
 /**
- * @brief TODO:
+ * @brief this function sets all neccesary values inside scanner buffer struct to it's initialization state and
+ * frees memmory for buffer
  * 
- * @param buf 
+ * @param buf pointer to scanner buffer structure
  */
 void scanner_buf_free(ScannerBuffer *buf);
 
 /**
- * @brief TODO:
+ * @brief this function inserts character to buffer and reallocates memmory if needed
  * 
- * @param buf 
- * @param ch 
- * @return true 
- * @return false 
+ * @param buf pointer to scanner buffer structure
+ * @param ch character to be stored
+ * @return true if reallocation was succesfull
+ * @return false if reallocation error occured
  */
 bool scanner_buf_insert(ScannerBuffer *buf, char ch);
 
 /**
- * @brief TODO:
+ * @brief this function returns next character from input file
+ * likewise to fgetc() function
  * 
- * @param opt 
- * @return int 
+ * @param opt scanner options struct pointer containing all necessary data
+ * for handling input code IFJ23
+ * @return ascii value of returned character
  */
 int get_next_char(ScannerOptions *opt);
 
 /**
- * @brief TODO:
+ * @brief main scanner function that contains finite state machine 
+ * and handles recognition of various tokens
  * 
- * @param parser_opt 
- * @return true 
- * @return false 
+ * @param parser_opt parser options struct pointer where token and other neccessary
+ *  data is being written to
+ * @return true if no lexical or internal error occured and proper token was recognized
+ * @return false if lexical or internal error occured
  */
 bool get_next_token(ParserOptions *parser_opt);
 
